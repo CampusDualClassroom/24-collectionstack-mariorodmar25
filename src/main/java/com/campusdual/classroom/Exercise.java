017cfe77d25558f970c5530b26152598ec8c3ba7
Mario Rodríguez Martínez <mario.rodriguez.martinez.lu@gmail.com>
2025-11-13 17:35:00 +0100

    Solved exercise 24

--- src/main/java/com/campusdual/classroom/Exercise.java
+++ src/main/java/com/campusdual/classroom/Exercise.java
@@ -4,14 +4,31 @@ import java.util.Stack;
 
 public class Exercise {
     public static Stack<String> createStack() {
+        Stack<String> stack = new Stack<>();
+        stack.push("Smith");
+        stack.push("Montessori");
+        stack.push("Peralta");
+        stack.push("House");
+        return stack;
+
+
 
     }
 
     public static void printAndEmptyStack(Stack<String> stack) {
+        while (!stack.isEmpty()){
+            String pop = stack.pop();
+            System.out.println(pop);
+
+        }
+
+
 
     }
 
     public static void main(String[] args) {
+        Stack<String> stack = createStack();
+        printAndEmptyStack(stack);
 
     }
 
